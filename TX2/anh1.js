2d7091e5cd205a293a30071aa31b5a0a095926f9
VanTugh <nguyenvantung07032005@gmail.com>
2025-08-17 15:57:11 +0700

    on web

--- TX2/anh1.js
+++ TX2/anh1.js
@@ -1,28 +1,3 @@
-// const bangGia = {
-//   6: 1000000,
-//   5: 800000,
-//   4: 700000,
-//   3: 600000,
-//   1: 400000,
-// };
-// function capnhatphong() {
-//   const loai = document.getElementById("loaiphong").value;
-//   document.getElementById("price").value = bangGia[loai] || " ";
-//   document.getElementById("result").value = " ";
-// }
-// function tinh() {
-//   const loai = document.getElementById("loaiphong").value;
-//   const gia = bangGia[loai];
-//   const ngay = document.getElementById("songaythue").value;
-
-//   if (!isNaN(loai) && !isNaN(gia) && !isNaN(ngay)) {
-//     const tien = ngay * gia;
-//     document.getElementById("result").value = tien.toLocaleString() + " VND ";
-//   } else {
-//     alert(" vui long nhap lai !");
-//   }
-// }
-
 const bangGia = {
   6: 1000000,
   5: 800000,
